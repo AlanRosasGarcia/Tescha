4ee4423795cc2d83b7e3fff1a9a3f6f1e56bf205
AlanRosasGarcia <turingrg18@gmail.com>
2022-03-11 23:50:36 -0600

    index.php

--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@ require __DIR__ . '../vendor/autoload.php';//requiriendo el autoload
 //__DIR__ es para crear rutas absolutas 
 $recuest = new App\Http\Recuest;//creando un objeto recuest 
 
-$recuest->send();
+$recuest->send(); 
