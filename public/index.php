54cf488026668bd9edd909825056be7c92a962c5
AlanRosasGarcia <turingrg18@gmail.com>
2022-03-11 23:15:58 -0600

    agregando fron controller

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// centralizando las peticiones 
+require __DIR__ . '../vendor/autoload.php';//requiriendo el autoload
+//__DIR__ es para crear rutas absolutas 
+$recuest = new App\Http\Recuest;//creando un objeto recuest 
+
+$recuest->send();
